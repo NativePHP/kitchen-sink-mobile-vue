040dc6897216fed97528e2eff5e2bf7262b5b55d
Shane Rosenthal <srosenthal82@gmail.com>
2026-02-10 16:52:01 -0500

    Updates

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('EdgePlayground');
+    return Inertia::render('Welcome');
 })->name('home');
 
 Route::get('/welcome', function () {
